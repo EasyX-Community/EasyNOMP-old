89d552e836ba59e8e3391fbc37b37f849ce18081
kthx <kthx@aurad.lan>
2019-08-09 07:00:39 +0000

    Update

---
+++ pool-restart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo ""
+echo "EasyNOMP Starting..."
+echo ""
+
+source ~/.bashrc
+source /etc/os-release
+
+## who am i? ##
+SCRIPTNAME="$(readlink -f ${BASH_SOURCE[0]})"
+BASEDIR="$(dirname $SCRIPTNAME)"
+
+## Okay, print it ##
+echo "Script name : $SCRIPTNAME"
+echo "Current working dir : $PWD"
+echo "Script location path (dir) : $BASEDIR"
+echo ""
+
+#pm2 del pool
+
+#pm2 start --name pool node -- --optimize_for_size --max-old-space-size=4096  "${BASEDIR}/init.js"
+
+pm2 restart pool
+
+renice -n -18 -p $(pidof node)
+renice -n -18 -p $(pidof nodejs)
+
+echo ""
+echo "Done!"
+echo ""
